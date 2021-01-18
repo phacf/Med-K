673a03154ba629a790a370c7cd2c096565006499
Cirineu Rodrigues <cirineurodrigues@hotmail.com>
2021-01-18 15:27:50 -0300

    Adiciona header com menu e menu mobile, e alteraçao nas rotas

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,9 @@
 import { Route, Switch } from "react-router-dom";
 import { AnimatePresence } from "framer-motion";
+import { useEffect, useState } from "react";
+import { useHistory, useLocation } from "react-router-dom";
+
+import Header from "../components/Header";
 
 import Login from "../pages/Login";
 import Register from "../pages/Register";
@@ -11,17 +15,42 @@ import DoctorExams from "../pages/DoctorExams";
 import DoctorRequest from "../pages/DoctorRequest";
 
 const Routes = () => {
+  const [isAuthenticated, setAuthentication] = useState(false);
+  const history = useHistory();
+  const location = useLocation();
+
+  useEffect(() => {
+    const token = window.localStorage.getItem("authToken");
+    if (token) {
+      setAuthentication(true);
+      if (location.pathname === "/") {
+        history.push("/");
+      }
+    }
+  }, [location.pathname, history]);
+
+  if (isAuthenticated === false) {
+    return (
+      <>
+        <AnimatePresence>
+          <Switch>
+            <Route exact path="/register">
+              <Register />
+            </Route>
+            <Route exact path="/login">
+              <Login />
+            </Route>
+          </Switch>
+        </AnimatePresence>
+      </>
+    );
+  }
+
   return (
     <>
       <AnimatePresence>
+        <Header menuItems={["Exames", "Consultas", "Pacientes"]} paths={[]} />
         <Switch>
-          <Route exact path="/register">
-            <Register />
-          </Route>
-          <Route exact path="/">
-            <Login />
-          </Route>
-
           <Route exact path="/doctor/request">
             <DoctorRequest />
           </Route>
