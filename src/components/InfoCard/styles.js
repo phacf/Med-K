914a1f76624edc6719c81d70759e91f1076b52a1
Davi Andrade <code.easy.31415@gmail.com>
2021-01-22 03:15:00 +0000

    Add new file

---
+++ src/components/InfoCard/styles.js
@@ -0,0 +1,23 @@
+import styled from "styled-components";
+
+import { colors } from "../../styles/colors";
+
+export const StyledCard = styled.div`
+  width: 100%;
+  padding: 10px;
+  margin: 0 auto;
+  margin-top: 20px;
+  border-radius: 10px;
+  border-bottom: 6px solid ${colors.blue3};
+  background-color: ${colors.blue1};
+
+  h1 {
+    color: ${colors.gray3};
+    font-size: 1rem;
+  }
+
+  p {
+    color: ${colors.gray2};
+    font-size: 1rem;
+  }
+`;
